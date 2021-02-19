564956e41817bc5576adf66350066d4e750df2fe
Campe <Campe@LAPTOP-F5OP52OL>
2021-02-18 20:20:07 -0600

    ESTORY IGUALANDO MIS DOS COPIAS, EL WORKING Y EL LOCAL REPOSITORY

---
+++ src/git/pkg67606/Git67606.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.pkg67606;
+
+/**
+ *
+ * @author Campe
+ */
+public class Git67606 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre= "JESUS ALFREDO";
+        String matricula="67606";
+        System.out.println(" SOY " + nombre + "Y MI MATRICULA ES" + matricula );
+        
+        //ACABO DE CREAR MI RESPOSITORIO LOCAL EN TEAM 
+        
+    }
+    
+}
