6e1ed8e784104049805aaa030f60be066e14ecec
danieljaimes <danieljaimes@192.168.0.9>
2021-02-19 20:24:40 -0600

    Primer commit

---
+++ Git061844/src/git061844/Git061844.java
@@ -0,0 +1,22 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package git061844;
+
+/**
+ *
+ * @author danieljaimes
+ */
+public class Git061844 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Git061844");
+    }
+    
+}
