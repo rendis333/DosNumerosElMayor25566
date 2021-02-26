fd7f9f6c0e3fc88e93aa42cdf268a964347b92b9
escuela <escuela@EC26>
2021-02-25 20:11:13 -0600

    comming de nombre

---
+++ Rodrigo25566/src/rodrigo25566/Rodrigo25566.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rodrigo25566;
+
+/**
+ *
+ * @author escuela
+ */
+public class Rodrigo25566 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        nombre = "rodrigo Roman Cruz Rendis";
+        System.out.println("nombre:" + nombre);
+    }
+    
+}
