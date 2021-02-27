1dfa697ca167273ad0a2c36fcf21fca9ff16ec7b
escuela <escuela@EC26>
2021-02-26 18:20:38 -0600

    comming de nombre

---
+++ Nombre25566/src/nombre25566/Nombre25566.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nombre25566;
+
+/**
+ *
+ * @author escuela
+ */
+public class Nombre25566 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String nombre;
+        nombre = "rodrigo Roman Cruz Rendis";
+        System.out.println("nombre:" + nombre);
+    }
+    
+}
